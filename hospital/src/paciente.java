778b1354f6cabde1ea9bbc4bfc5839baab592072
tru17189 <32879914+tru17189@users.noreply.github.com>
2018-04-05 18:11:37 -0600

    2

--- hospital/src/paciente.java
+++ hospital/src/paciente.java
@@ -3,18 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hospital;
 
 /**
  *
  * @author ottoalexander
  */
-public class Hospital {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+public class paciente {
+     public static void main(String[] args) {
         // TODO code application logic here
     }
     
